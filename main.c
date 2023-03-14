410bef20f47175ec2735e7a72277768780fa92c1
Benjaamin <87545581+bseligman@users.noreply.github.com>
2023-03-13 21:13:26 -0400

    fix

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ void DrawSprite(char* sprite, int x, int y, int scale)
 
    file = fopen(sprite, "r");
 
-    code = malloc(1000);
+    code = malloc(2000);
     while ((c = fgetc(file)) != EOF)
     {
         code[n++] = (char) c;
@@ -85,17 +85,17 @@ void DrawSprite(char* sprite, int x, int y, int scale)
  }
 
 ///// LOAD ROOM FUNCTION
-void LoadRoom(char* sprite, int scale)
+void LoadRoom(char* sprite, int scale, int np_x, int np_y)
 {
 	// Load Vars
    FILE *file;
    char *code;
    size_t n = 0;
-   int c,x=0,y=0;
+   int c,x=np_x,y=np_y;
 
    // Open Map Visual Data
    file = fopen(sprite, "r");
-    code = malloc(1500);
+    code = malloc(10000);
     while ((c = fgetc(file)) != EOF)
     {
         code[n++] = (char) c;
@@ -124,7 +124,7 @@ void LoadRoom(char* sprite, int scale)
    		}
    	}
    	if(code[i] == '0'){
-   	   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+   	   SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
    		for(int c=0;c<scale;c++){
    			SDL_RenderDrawPoint(renderer, x, y+c);
    		}   	
@@ -162,7 +162,7 @@ int getCords(char* sprite, int scale, int inpx, int inpy)
 
    // Open Map Visual Data
    file = fopen(sprite, "r");
-    code = malloc(1500);
+    code = malloc(10000);
     while ((c = fgetc(file)) != EOF)
     {
         code[n++] = (char) c;
@@ -174,7 +174,7 @@ int getCords(char* sprite, int scale, int inpx, int inpy)
    fclose(file);
 
    //arrays need to be very big to hold room data jesus christ
-   int anaray[441][441];
+   int anaray[871][871];
 
    //Go through and stuff
    int ogx = x;
@@ -224,20 +224,29 @@ int getCords(char* sprite, int scale, int inpx, int inpy)
 
 
 
-// Main Starting Function
+// MAIN LOOP / STARTING FUNC
 int main( int argc, char* args[] )
 {
+
+	// COLLISION / WORLD SCALE
+	int cscale=22;
+
 	//// PLAYER VARS
 	char* activer = "room1.txt";
-	int xpos = 10;
-	int ypos = 10;
+	int xpos = 20;
+	int ypos = 20;
 	//Create window, base=128 (scaled*5)
 	SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
 
-	// ROOM STATUS VARS
+	// ROOM STATUS VARS 
 	int loadroom1 = 0;
 
-	//// OVERWORLD LOOP
+	// CAMERA VARS
+	int camxpos = 256;
+	int camypos= 256;
+
+
+	////////////////////////////////// OVERWORLD LOOP
 	while (true) {
 	// Get next event
 	SDL_Event event;
@@ -251,12 +260,9 @@ int main( int argc, char* args[] )
 	//Draw Background
 	fillscreen(0,0,0);
 
-	// COLLISION SCALE
-	int cscale=10;
-
 	///// LOAD ROOM MANAGER
 	// Room Load
-	LoadRoom(activer,cscale);
+	LoadRoom(activer,cscale, xpos, ypos);
 	if (loadroom1 == 1) {
 		activer = "room2.txt";
 		loadroom1=0;
@@ -271,57 +277,65 @@ int main( int argc, char* args[] )
 	}
 
    //Draw Player
-	DrawSprite("chartest.txt",xpos,ypos,1);
+	DrawSprite("chartest.txt",256,256,4);
 
 	//// DEBUG MODE
 	// PLAYER POSITION
 	// XY NEXT+1
-	if (getCords(activer,cscale,xpos+3,ypos)==0) {
+	if (getCords(activer,cscale,camxpos+3,camypos)==0) {
 		DrawSprite("0.txt",20,200,4);
 	}
-	if (getCords(activer,cscale,xpos+3,ypos)==1) {
+	if (getCords(activer,cscale,camxpos+3,camypos)==1) {
 		DrawSprite("1.txt",20,200,4);
 	}
-	if (getCords(activer,cscale,xpos+3,ypos)==2) {
+	if (getCords(activer,cscale,camxpos+3,camypos)==2) {
 		DrawSprite("2.txt",20,200,4);
 	}
-	if (getCords(activer,cscale,xpos+3,ypos)==4) {
+	if (getCords(activer,cscale,camxpos+3,camypos)==4) {
 		DrawSprite("4.txt",20,200,4);
 	}
 
-	//UP
+
+	// DOWN
 	if(ch == 119)
 	{
-		if(getCords(activer,cscale,xpos,ypos-3) == 0) {
-			ypos=ypos-3;
+		if(getCords(activer,cscale,camxpos,camypos+3) == 0) {
+			ypos=ypos+cscale/2;
+			camypos=ypos+cscale/2;
 		}
 	}
 
-	// DOWN
+	// UP
 	if(ch == 115)
 	{
-		if(getCords(activer,cscale,xpos,ypos+3) == 0) {
-			ypos=ypos+3;
+		if(getCords(activer,cscale,camxpos,camypos-3) == 0) {
+			ypos=ypos-cscale/2;
+			camypos=ypos-cscale/2;
 		}
 	}
  
-	//LEFT
+	// RIGHT
 	if(ch == 97)
 	{
-		if(getCords(activer,cscale,xpos-3,ypos) == 0) {
-			xpos=xpos-3;
+		if(getCords(activer,cscale,camxpos+3,camypos) == 0) {
+			xpos=xpos+cscale/2;
+			camxpos=xpos+cscale/2;
 		}
 	}
 
-	//RIGHT
+	// LEFT
 	if(ch == 100)
 	{
 		// Check boundry collision
-		if(getCords(activer,cscale,xpos+3,ypos) == 0) {
-			xpos=xpos+3;
+		if(getCords(activer,cscale,camxpos-3,camypos) == 0) {
+			xpos=xpos-cscale/2;
+			camxpos=xpos-cscale/2;
 		}	
 	}	
 
+//BEN NOTE: OFFSET OF Y IS WRONG AND X, BUT IS ALMOST THERE!
+	// THE OFFSET!
+	
 	// Refresh the render/frame
 	SDL_RenderPresent(renderer);
 
